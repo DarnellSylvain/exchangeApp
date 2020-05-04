3fac988caae002d01468c9022138ce3eb254c27e
DarnellS <darnellsylvain@gmail.com>
2020-05-04 18:55:14 +0100

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const api = "https://api.exchangeratesapi.io/latest";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [currencyData, setCurrencyData] = useState({});
+  const [selectValue, setSelectValue] = useState("");
+
+  const search = (event) => {
+    event.preventDefault();
+    console.log("Clicked");
+    fetch(`${api}?base=${selectValue}`)
+      .then((res) => res.json())
+      .then((response) => {
+        setCurrencyData(response);
+        console.log(response);
+        console.log(selectValue);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="currency-box">
+        <form onSubmit={search}>
+          <div className="inputs">
+            <select onChange={(e) => setSelectValue(e.target.value)}>
+              <option value="none">Currency</option>
+              <option value="GBP">GBP</option>
+              <option value="CAD">CAD</option>
+              <option value="USD">USD</option>
+            </select>
+            <input
+              className="input-box"
+              placeholder="Enter Number"
+              min="0"
+              type="number"
+              value={value}
+              name="value"
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button>Convert</button>
+          </div>
+        </form>
+        {typeof currencyData.rates != "undefined" ? (
+          <div>
+            <span>{value * currencyData.rates.USD}</span>
+            <span> Hello</span>
+            <span> Hello</span>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
